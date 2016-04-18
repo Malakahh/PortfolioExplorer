8e8af520a1093a8f4b2d6554167f315cf8146dc5
Malakahh <Malakahh@users.noreply.github.com>
2016-04-18 10:35:04 +0200

    StageManager

---
+++ PortfolioExplorer/Assets/Stages/StageManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+public class StageManager : MonoBehaviour {
+    public enum Stage { Library, EvelynsAdventure }
+
+    public static StageManager Instance;
+
+    public List<StageObjectKeyValue> stages = new List<StageObjectKeyValue>();
+
+    int currentStageId = -1;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+
+    void Start()
+    {
+        ChangeStage(Stage.Library);
+    }
+
+    public void ChangeStage(Stage s)
+    {
+        if (currentStageId > -1)
+        {
+            stages[currentStageId].StageObject.SetActive(false);
+        }
+
+        currentStageId = stages.FindIndex(x => x.Stage == s);
+        stages[currentStageId].StageObject.SetActive(true);
+    }
+
+    [System.Serializable]
+    public class StageObjectKeyValue
+    {
+        public Stage Stage;
+        public GameObject StageObject;
+    }
+}
+
+
