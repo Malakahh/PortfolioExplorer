dc4e69a9c883b4ee45967389045f4eff0d8f2c56
Malakahh <Malakahh@users.noreply.github.com>
2016-04-24 21:28:11 +0200

    Added TextureManager

---
+++ PortfolioExplorer/Assets/GenerelPurpose Scripts/TextureManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TextureManager : MonoBehaviour {
+    public static TextureManager Instance;
+
+    public NigelsAdventureContainer NigelsAdventure = new TextureManager.NigelsAdventureContainer();
+
+    void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(Instance);
+    }
+    
+    [System.Serializable]
+    public class NigelsAdventureContainer
+    {
+        public List<Texture2D> Tiles;
+    }
+}
